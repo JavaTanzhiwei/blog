ca8864bcc0a34cc4e494ab6d620b8a5a60b5e3e6
tanzhiwei <tanzhiwei0526@163.com>
2023-04-13 09:56:39 +0800

    初始化

--- vdoing/components/NavLinks.vue
+++ vdoing/components/NavLinks.vue
@@ -1,53 +1,53 @@
 <template>
   <nav
-    class="nav-links"
-    v-if="userLinks.length || repoLink"
+      class="nav-links"
+      v-if="userLinks.length || repoLink"
   >
     <!-- user links -->
     <div
-      class="nav-item"
-      v-for="item in userLinks"
-      :key="item.link"
+        class="nav-item"
+        v-for="item in userLinks"
+        :key="item.link"
     >
       <DropdownLink
-        v-if="item.type === 'links'"
-        :item="item"
+          v-if="item.type === 'links'"
+          :item="item"
       />
       <NavLink
-        v-else
-        :item="item"
+          v-else
+          :item="item"
       />
     </div>
 
     <!-- repo link -->
     <a
-      v-if="repoLink"
-      :href="repoLink"
-      class="repo-link"
-      target="_blank"
-      rel="noopener noreferrer"
+        v-if="repoLink"
+        :href="repoLink"
+        class="repo-link"
+        target="_blank"
+        rel="noopener noreferrer"
     >
       {{ repoLabel }}
-      <OutboundLink />
+      <OutboundLink/>
     </a>
   </nav>
 </template>
 
 <script>
 import DropdownLink from '@theme/components/DropdownLink.vue'
-import { resolveNavLinkItem } from '../util'
+import {resolveNavLinkItem} from '../util'
 import NavLink from '@theme/components/NavLink.vue'
 
 export default {
-  components: { NavLink, DropdownLink },
+  components: {NavLink, DropdownLink},
 
   computed: {
-    userNav () {
+    userNav() {
       return this.$themeLocaleConfig.nav || this.$site.themeConfig.nav || []
     },
 
-    nav () {
-      const { locales } = this.$site
+    nav() {
+      const {locales} = this.$site
       if (locales && Object.keys(locales).length > 1) {
         const currentLink = this.$page.path
         const routes = this.$router.options.routes
@@ -70,7 +70,7 @@ export default {
                 link = path
               }
             }
-            return { text, link }
+            return {text, link}
           })
         }
         return [...this.userNav, languageDropdown]
@@ -78,7 +78,7 @@ export default {
       return this.userNav
     },
 
-    userLinks () {
+    userLinks() {
       return (this.nav || []).map(link => {
         return Object.assign(resolveNavLinkItem(link), {
           items: (link.items || []).map(resolveNavLinkItem)
@@ -86,24 +86,24 @@ export default {
       })
     },
 
-    repoLink () {
-      const { repo } = this.$site.themeConfig
+    repoLink() {
+      const {repo} = this.$site.themeConfig
       if (repo) {
         return /^https?:/.test(repo)
-          ? repo
-          : `https://github.com/${repo}`
+            ? repo
+            : `https://gitee.com/${repo}`
       }
       return null
     },
 
-    repoLabel () {
+    repoLabel() {
       if (!this.repoLink) return
       if (this.$site.themeConfig.repoLabel) {
         return this.$site.themeConfig.repoLabel
       }
 
       const repoHost = this.repoLink.match(/^https?:\/\/[^/]+/)[0]
-      const platforms = ['GitHub', 'GitLab', 'Bitbucket']
+      const platforms = ['GitHub', 'GitLab', 'Bitbucket', 'Gitee']
       for (let i = 0; i < platforms.length; i++) {
         const platform = platforms[i]
         if (new RegExp(platform, 'i').test(repoHost)) {
@@ -120,33 +120,42 @@ export default {
 <style lang="stylus">
 .nav-links
   display inline-block
+
   a
     line-height 1.4rem
     color inherit
+
     &:hover, &.router-link-active
       color $accentColor
+
   .nav-item
     position relative
     display inline-block
     margin-left 1.5rem
     line-height 2rem
+
     &:first-child
       margin-left 0
+
   .repo-link
     margin-left 1.5rem
+
 // 959
 @media (max-width $MQNarrow)
   .nav-links
     .nav-item
       margin-left 1.2rem
+
 @media (max-width $MQMobile)
   .nav-links
     .nav-item, .repo-link
       margin-left 0
+
 @media (min-width $MQMobile)
   .nav-links a
     &:hover, &.router-link-active
       color var(--textColor)
+
   .nav-item > a:not(.external)
     &:hover, &.router-link-active
       margin-bottom -2px
