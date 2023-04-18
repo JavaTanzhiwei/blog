96938977a11bd4c2730929d9ca350cfdd5a57b54
tanzhiwei <tanzhiwei0526@163.com>
2023-04-18 17:14:40 +0800

    编写博客

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -21,7 +21,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
             description: '好记性不如烂笔头',
         }
     },
-    base: '/blog/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+    // base: '/blog/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
 
     // 主题配置
     themeConfig: {
